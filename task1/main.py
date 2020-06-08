11b849cfb6dac633d9f74de9b9d99b85a659ce5b
amilalizada <elizade_amil@inbox.ru>
2020-06-08 19:45:45 +0400

    tasl_1

---
+++ task1/main.py
@@ -0,0 +1,10 @@
+import datetime
+
+print(datetime.datetime.today())
+print(datetime.datetime.today().strftime('%Y'))
+print(datetime.datetime.today().strftime('%B'))
+print(datetime.datetime.today().strftime('%W'))
+print(datetime.datetime.today().strftime('%w'))
+print(datetime.datetime.today().strftime('%j'))
+print(datetime.datetime.today().strftime('%-d'))
+print(datetime.datetime.today().strftime('%A'))
